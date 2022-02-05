40b2c2c4de7ff8bf7c1779b0cbda53293c36c1fe
orbrew <andreworbine@gmail.com>
2022-02-04 23:57:00 -0500

    html layout and styling

--- content.js
+++ content.js
@@ -32,8 +32,6 @@ function openComments() {
   commentsBoxHeader.id = "comments-box-header";
   commentsContainer.id = "comments-container";
 
-  commentsBoxHeader.innerHTML = "Drag here";
-
   //commentsContainer.className = "style-scope ytd-watch-flexy";
   if (theme) {
     commentsContainer.className = "dark";
@@ -43,10 +41,17 @@ function openComments() {
   console.log(origElement)
 
   contentElement.appendChild(commentsContainer);
-
+  commentsContainer.appendChild(commentsBoxHeader);
+  commentsBoxHeader.innerHTML = 'POGGERS'
   commentsContainer.appendChild(commentsBox);
-  //document.getElementById("comments-box").appendChild(commentsBoxHeader);
   document.getElementById("comments-box").appendChild(commentsContent);
+
+  var headerCloseButton = document.createElement("button");
+  headerCloseButton.id = "comments-close-button";
+  headerCloseButton.innerHTML = '&times'
+  commentsBoxHeader.append(headerCloseButton);
+
+
 
   dragEle();
 }
@@ -78,7 +83,6 @@ function getPrimary() {
 
 function dragEle() {
   var commentsBox = document.getElementById("comments-box");
-  //var commentsBoxHeader = document.getElementById("comments-box-header");
   var commentsContainer = document.getElementById("comments-container");
   var initX, initY, mousePressX, mousePressY;
 
@@ -120,69 +124,4 @@ function disableSelect(event) {
   event.preventDefault();
 } 
 
-/* function dragTest() {
-  var dragItem = document.getElementById("comments-box");
-  var container = document.getElementById("comments-box");
-
-  var active = false;
-  var currentX;
-  var currentY;
-  var initialX;
-  var initialY;
-  var xOffset = 0;
-  var yOffset = 0;
-
-  container.addEventListener("touchstart", dragStart, false);
-  container.addEventListener("touchend", dragEnd, false);
-  container.addEventListener("touchmove", drag, false);
-
-  container.addEventListener("mousedown", dragStart, false);
-  container.addEventListener("mouseup", dragEnd, false);
-  container.addEventListener("mousemove", drag, false);
-
-  function dragStart(e) {
-    if (e.type === "touchstart") {
-      initialX = e.touches[0].clientX - xOffset;
-      initialY = e.touches[0].clientY - yOffset;
-    } else {
-      initialX = e.clientX - xOffset;
-      initialY = e.clientY - yOffset;
-    }
-
-    if (e.target === dragItem) {
-      active = true;
-    }
-  }
-
-  function dragEnd(e) {
-    initialX = currentX;
-    initialY = currentY;
-
-    active = false;
-  }
-
-  function drag(e) {
-    if (active) {
-      e.preventDefault();
-
-      if (e.type === "touchmove") {
-        currentX = e.touches[0].clientX - initialX;
-        currentY = e.touches[0].clientY - initialY;
-      } else {
-        currentX = e.clientX - initialX;
-        currentY = e.clientY - initialY;
-      }
-
-      xOffset = currentX;
-      yOffset = currentY;
-
-      setTranslate(currentX, currentY, dragItem);
-    }
-  }
-
-  function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-  }
-}
- */
 AddCommentsButton();
